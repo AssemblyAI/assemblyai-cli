e55f804afd78b71e0f7b68b2b307269362587f45
Francisco Castillo <fcastillo90@gmail.com>
2022-09-28 09:46:27 +0200

    refactor

--- cmd/root.go
+++ cmd/root.go
@@ -1,51 +1,134 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 
+	badger "github.com/dgraph-io/badger/v3"
 	"github.com/spf13/cobra"
 )
 
 
+var AAITokenEnvName = "ASSMEBLYAI_TOKEN"
+var AAIURL = "https://api.assemblyai.com/v2"
 
-// rootCmd represents the base command when called without any subcommands
+
 var rootCmd = &cobra.Command{
 	Use:   "cli",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+		examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		Cobra is a CLI library for Go that empowers applications.
+		This application is a tool to generate the needed files
+		to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("help section")
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
+// ConfigureDatabase : Change default options for startup here
+func GetDatabaseConfig() badger.Options {
+	badgerCfg := badger.DefaultOptions("/tmp/badger")
+	badgerCfg.Logger = nil
+	return badgerCfg
+}
 
+func HandleError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func GetOpenDatabase() *badger.DB {
+	badgerOptions := GetDatabaseConfig()
+	db, err := badger.Open(badgerOptions)
+	HandleError(err)
+	return db
+}
+
+func GetStoredToken(db *badger.DB) string {
+	var valCopy []byte
+	err := db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(AAITokenEnvName))
+		HandleError(err)
+
+		err = item.Value(func(val []byte) error {
+			valCopy = append([]byte{}, val...)
+			return nil
+		})
+		return nil
+	})
+	HandleError(err)
+
+	return string(valCopy)
+}
+
+func QueryApi(token string, path string, method string, body io.Reader) []byte {
+	resp, err := http.NewRequest(method, AAIURL + path, body)
+	HandleError(err)
+
+	resp.Header.Add("Accept", "application/json")
+	resp.Header.Add("Authorization", token)
+	
+	response, err := http.DefaultClient.Do(resp)
+	HandleError(err)
+	defer response.Body.Close()
+	
+	responseData, err := ioutil.ReadAll(response.Body)
+	HandleError(err)
+
+	return responseData
+}
+
+func checkIfTokenValid(token string) CheckIfTokenValidResponse {
+	var funcResponse CheckIfTokenValidResponse
+
+	response := QueryApi(token, "/account", "GET", nil)
+
+	if response == nil {
+		return funcResponse
+	}
+
+	var result Account
+	if err := json.Unmarshal(response, &result); err != nil {
+			fmt.Println("Can not unmarshal JSON")
+	}
+	funcResponse.IsVerified = result.IsVerified
+	funcResponse.CurrentBalance = fmt.Sprintf("%f", result.CurrentBalance.Amount)
+	
+	return funcResponse
+}
+
+type CheckIfTokenValidResponse struct {
+	IsVerified     bool   `json:"is_verified"`
+	CurrentBalance string `json:"current_balance"`
+}
+
+type Account struct {
+	IsVerified     bool           `json:"is_verified"`    
+	CurrentBalance CurrentBalance `json:"current_balance"`
+}
+
+type CurrentBalance struct {
+	Amount   float64 `json:"amount"`  
+	Currency string  `json:"currency"`
+}
--- cmd/transcribe.go
+++ cmd/transcribe.go
@@ -6,7 +6,6 @@ package cmd
 import (
 	"fmt"
 
-	badger "github.com/dgraph-io/badger/v3"
 	"github.com/spf13/cobra"
 )
 
@@ -15,39 +14,17 @@ var transcribeCmd = &cobra.Command{
 	Use:   "transcribe",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+		and usage of using your command. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Cobra is a CLI library for Go that empowers applications.
+		This application is a tool to generate the needed files
+		to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
-		if err != nil {
-			fmt.Println(err)
-		}
+		fmt.Println("transcribe called")
+		db := GetOpenDatabase()
+		token := GetStoredToken(db)
+		fmt.Printf("The answer is: %s\n", token)
 		defer db.Close()
-
-		err = db.View(func(txn *badger.Txn) error {
-			item, err := txn.Get([]byte(AAITokenEnvName))
-			if err != nil {
-				fmt.Println(err)
-			}
-			var valCopy []byte
-			err = item.Value(func(val []byte) error {
-				valCopy = append([]byte{}, val...)
-				return nil
-			})
-			valCopy, err = item.ValueCopy(nil)
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Printf("The answer is: %s\n", valCopy)
-		
-			return nil
-		})
-
-    fmt.Printf("Database Password: ")
-		
 	},
 }
 
